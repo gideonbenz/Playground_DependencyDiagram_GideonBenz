afd6964e9d9636fd5a7dd07f8c8b347e7339add9
Gideon Benz <gideonbenz@gmail.com>
2022-02-10 14:20:34 +0700

    add LocalFeedLoader, use Remote and Local concrete type on FeedViewController, update visualisation diagram

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,24 +8,42 @@ protocol FeedLoader {
     func loadFeed(completion: (([String]) -> Void) -> Void)
 }
 
+struct Reachability {
+    static let networkAvailable = false
+}
+
 class FeedViewController: UIViewController {
-    var loader: FeedLoader!
+    var remote: RemoteFeedLoader!
+    var local: LocalFeedLoader!
     
-    convenience init(loader: FeedLoader) {
+    convenience init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
         self.init()
-        self.loader = loader
+        self.remote = remote
+        self.local = local
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loader.loadFeed { loadedItems in
-            // update UI
+        if Reachability.networkAvailable {
+            remote.loadFeed { loadedItems in
+                // update UI
+            }
+        } else {
+            local.loadFeed { loadedItems in
+                // update UI
+            }
         }
     }
 }
 
 class RemoteFeedLoader: FeedLoader {
+    func loadFeed(completion: (([String]) -> Void) -> Void) {
+        // do something
+    }
+}
+
+class LocalFeedLoader: FeedLoader {
     func loadFeed(completion: (([String]) -> Void) -> Void) {
         // do something
     }
